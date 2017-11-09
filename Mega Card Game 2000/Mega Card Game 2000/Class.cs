885491ef605f416c535507fd8460b39ee3d3d175
thaceul <33332888+thaceul@users.noreply.github.com>
2017-11-10 08:32:16 +1100

    Add files via upload

---
+++ Mega Card Game 2000/Mega Card Game 2000/Class.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mega_Card_Game_2000
+{
+    class Character
+    {
+        public string name;
+        public int health;
+        public int damage;
+
+        public void Damage(Character pTarget, int pDamage)
+        {
+            pTarget.health -= pDamage;
+        }
+    }
+
+    class NonPlayerCharacter : Character
+    {
+        public NonPlayerCharacter(string pName, int pHealth, int pDamage)
+        {
+            name = pName;
+            health = pHealth;
+            damage = pDamage;
+        }
+    }
+
+    class PlayerCharacter : Character
+    {
+        PlayerCharacterType type;
+
+        public PlayerCharacter(string pName, int pHealth, int pDamage, string pType)
+        {
+            name = pName;
+            health = pHealth;
+            damage = pDamage;
+
+            switch (pType)
+            {
+                case "warrior":
+                    type = new Warrior(this);
+                    break;
+                case "thief":
+                    type = new Thief(this);
+                    break;
+                case "mage":
+                    type = new Mage(this);
+                    break;
+            }
+        }
+
+        public void AttackSpecial(NonPlayerCharacter pTarget)
+        {
+            type.AttackSpecial(pTarget);
+        }
+    }
+
+    class PlayerCharacterType
+    {
+        public PlayerCharacter pc;
+        public Random random = new Random();
+
+        public virtual void AttackSpecial(NonPlayerCharacter pTarget)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Warrior : PlayerCharacterType
+    {
+        public Warrior(PlayerCharacter pPlayerCharacter)
+        {
+            pc = pPlayerCharacter;
+        }
+
+        public override void AttackSpecial(NonPlayerCharacter pTarget)
+        {
+            if (random.Next(1, 4) == 1)
+            {
+                pc.Damage(pTarget, pc.damage * 3);
+            }
+        }
+    }
+
+    class Thief : PlayerCharacterType
+    {
+        public Thief(PlayerCharacter pPlayerCharacter)
+        {
+            pc = pPlayerCharacter;
+        }
+
+        public override void AttackSpecial(NonPlayerCharacter pTarget)
+        {
+            if (random.Next(1, 4) == 1)
+            {
+                pc.Damage(pTarget, pc.damage * 2);
+            }
+            else
+            {
+                pc.Damage(pTarget, pc.damage / 2);
+            }
+        }
+    }
+
+    class Mage : PlayerCharacterType
+    {
+        public Mage(PlayerCharacter pPlayerCharacter)
+        {
+            pc = pPlayerCharacter;
+        }
+
+        public override void AttackSpecial(NonPlayerCharacter pTarget)
+        {
+            if (random.Next(1, 3) == 1)
+            {
+                pc.Damage(pTarget, pc.damage * 4);
+            }
+            else
+            {
+                pc.Damage(pc, pc.damage);
+            }
+        }
+    }
+}
+
---
+++ Mega Card Game 2000/Mega Card Game 2000/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mega_Card_Game_2000
+{
+    public class Character
+    {
+        public int currentHealthPoints { get; set; }
+        public int maxHealthPoints { get; set; }
+
+        public Character(int health, int maxHealth)
+        {
+            currentHealthPoints = health;
+            maxHealthPoints = maxHealth;
+        }
+    }
+
+    public class PlayerCharacter
+    {
+        private char characterName { get; set; }
+        private int normalAttack { get; set; }
+        private int specialAttack { get; set; }
+        private PlayerCharacter(char Name, int NormalAttack, int SpeacialAttack)
+        {
+            characterName = Name;
+            normalAttack = NormalAttack;
+            specialAttack = SpeacialAttack;
+        }
+    }
+
+    public class GameController
+    {
+        private int enemy { get; set; }
+        private int player { get; set; }
+        private GameController(int NonPlayerCharacter, int PlayerCharacter)
+        {
+            enemy = NonPlayerCharacter;
+            player = PlayerCharacter;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        frameMainWindow.Navigate(new Battle());
+    }
+
+
+}
+    
+
+       
+
+
